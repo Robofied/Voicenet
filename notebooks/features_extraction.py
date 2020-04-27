d6fea6a2f20e86d22c8bf54526fa33299059139f
ridhimagarg <garg.ridhima72@gmail.com>
2020-04-27 18:36:31 +0530

    max length for mfcc features

--- notebooks/features_extraction.py
+++ notebooks/features_extraction.py
@@ -20,6 +20,7 @@ class mfcc_features(object):
         combined_feature      = np.hstack((mfcc_feature, deltas, double_deltas))
 
         print(combined_feature.shape)
+        print(len(combined_feature))
         return combined_feature
 
 if __name__ == "__main__":
