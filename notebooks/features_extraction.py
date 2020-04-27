844c545d9c6576acc1d8d1d09875b56d71c7e2a0
ridhimagarg <garg.ridhima72@gmail.com>
2020-04-27 18:33:37 +0530

    working on creating df

--- notebooks/features_extraction.py
+++ notebooks/features_extraction.py
@@ -6,22 +6,23 @@ import numpy as np
 
 class mfcc_features(object):
 
-    def __init__(self, filename, RAW_DATA_DIR= '../data/raw/ST-AEDS'):
-        self.filename = filename
+    def __init__(self, RAW_DATA_DIR= '../data/raw/ST-AEDS'):
+        # self.filename = filename
         self.RAW_DATA_DIR = RAW_DATA_DIR
 
-    def get_features(self):
+    def get_features(self, filename):
 
-        (rate,sig) = wav.read(os.path.join(self.RAW_DATA_DIR, self.filename))
+        (rate,sig) = wav.read(os.path.join(self.RAW_DATA_DIR, filename))
         mfcc_feature = mfcc(sig,rate, lowfreq=0,)
         mfcc_feature  = preprocessing.scale(mfcc_feature)
         deltas        = delta(mfcc_feature, 2)
         double_deltas = delta(deltas, 2)
         combined_feature      = np.hstack((mfcc_feature, deltas, double_deltas))
 
+        print(combined_feature.shape)
         return combined_feature
 
 if __name__ == "__main__":
 
-    mfcc_features = mfcc_features('f0001_us_f0001_00001.wav')
-    mfcc_features.get_features()
+    mfcc_features = mfcc_features()
+    mfcc_features.get_features('f0001_us_f0001_00001.wav')
