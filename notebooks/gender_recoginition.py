0f86f4560b553978fa042f80d19a5c32996f6d7c
ridhimagarg <garg.ridhima72@gmail.com>
2020-05-07 18:02:07 +0530

    some changed

---
+++ notebooks/gender_recoginition.py
@@ -0,0 +1,26 @@
+## Importing libraries
+
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os, sys, wave
+from sklearn.preprocessing import LabelEncoder
+
+import scipy.io.wavfile as wav
+from scipy.io.wavfile import read
+from python_speech_features import delta 
+from python_speech_features import mfcc
+from python_speech_features import logfbank
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.mixture import GaussianMixture
+from sklearn.metrics import accuracy_score
+
+
+## Our-defined packages
+from utils import label_encoder
+from features_extraction import mfcc_features
+
+
+DATA_RAW_DIR = '../data/raw/ST-AEDS'
