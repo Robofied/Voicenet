486607d407e72fac249c577106ea6d284837b996
ridhimagarg <garg.ridhima72@gmail.com>
2020-06-29 16:20:17 +0530

    gender identification

--- gender_recogintion.py
+++ gender_recogintion.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+import os
 
 from utils.features_extraction import mfcc_features
 
@@ -10,6 +11,8 @@ MALE_GMM_MODELFILE = 'males_gmm_model.gmm'
 
 def identify_gender(female_model, male_model, vector):
     
+    winner = ''
+    
     is_female_score = np.array(female_model.score(vector))
     is_female_log_likelihood = is_female_score.sum()
     
@@ -17,13 +20,27 @@ def identify_gender(female_model, male_model, vector):
     is_male_log_likelihood = is_male_score.sum()
     
     print("Female Likelihood {0}".format(is_female_log_likelihood))
-    print("Male Likelihood {0}".format()(is_male_log_likelihood))
+    print("Male Likelihood {0}".format(is_male_log_likelihood))
+        
     
-    winner = 'Male' if is_male_log_likelihood > is_female_log_likelihood else 'Female'
+    if is_male_log_likelihood > is_female_log_likelihood:
+        winner = "Male" 
+    else:
+        winner = "Female"
     
     return winner
 
 mfccfeatures = mfcc_features()
+
+file = 'data/raw/ST-AEDS/TestingData/females/f0001_us_f0001_00005.wav'
+
+female_model = pickle.load(open(os.path.join(MODEL_DIR, FEMALE_GMM_MODELFILE),'rb'))
+male_model = pickle.load(open(os.path.join(MODEL_DIR, MALE_GMM_MODELFILE), 'rb'))
+
+vector = mfccfeatures.get_features(file)
+
+winner = identify_gender(female_model,male_model,vector)
+print(winner)
 
 
 
